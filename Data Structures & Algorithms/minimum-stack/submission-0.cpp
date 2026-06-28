1b871a48af21ab093fd0f52dddcc201f74483ac5
Abdelkareem Yousef <111906953+Abdelkareem-Yousef@users.noreply.github.com>
2026-06-28 20:08:15 +0300

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,27 @@
+class MinStack {
+public:
+    stack <pair<int,int>> st;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        int minvalue = st.empty() ? val : min(st.top().second , val );
+        st.push({val,minvalue});
+        
+    }
+    
+    void pop() {
+        st.pop();
+    }
+    
+    int top() {
+        return st.top().first;
+        
+    }
+    
+    int getMin() {
+        return st.top().second;
+        
+    }
+};
